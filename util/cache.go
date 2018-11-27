4ff4b3b251749ee7421755e085a439c3588e3a1f
justwowkl <45342259+justwowkl@users.noreply.github.com>
2018-11-27 22:53:51 +0900

    Create cache.go

---
+++ util/cache.go
@@ -0,0 +1,20 @@
+package util
+
+import (
+	"fmt"
+
+	"github.com/go-redis/redis"
+)
+
+// CacheInit init cache
+func CacheInit() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+	// Output: PONG <nil>
+}
