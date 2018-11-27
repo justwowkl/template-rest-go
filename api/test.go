57bec97c0ad48782241175992c4627e7926b08b7
justwowkl <45342259+justwowkl@users.noreply.github.com>
2018-11-27 22:53:17 +0900

    Update test.go

--- api/test.go
+++ api/test.go
@@ -6,20 +6,22 @@ import (
 	"github.com/labstack/echo"
 )
 
-// Test test api
-func Test(c echo.Context) error {
+// PubTest test api
+func PubTest(c echo.Context) error {
 
 	// payload scheme
 	type requestScheme struct {
-		Name string `json:"name" validate:"required"`
-		Msg  string `json:"msg" validate:"required"`
-		Num  int    `json:"num" validate:"required"`
+		Name  string `json:"name" validate:"required"`
+		Msg   string `json:"msg" validate:"required"`
+		Num   int    `json:"num" validate:"required"`
+		Query string `json:"query"`
 	}
 	type responseScheme struct {
-		Name string `validate:"required"`
-		Msg  string `validate:"required"`
-		IP   string `validate:"required"`
-		Num  int    `validate:"required"`
+		Name  string `validate:"required"`
+		Msg   string `validate:"required"`
+		IP    string `validate:"required"`
+		Num   int    `validate:"required"`
+		Query string
 	}
 
 	// verify request payload
@@ -34,10 +36,11 @@ func Test(c echo.Context) error {
 
 	// verify response payload
 	responseJSON := &responseScheme{
-		Name: requestJSON.Name,
-		Msg:  requestJSON.Msg,
-		Num:  requestJSON.Num,
-		IP:   c.RealIP(),
+		Name:  requestJSON.Name,
+		Msg:   requestJSON.Msg,
+		Num:   requestJSON.Num,
+		IP:    c.RealIP(),
+		Query: requestJSON.Query,
 	}
 	if err := c.Validate(responseJSON); err != nil {
 		return c.String(http.StatusInternalServerError, err.Error())
