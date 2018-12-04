416b9dd364856b25955f44f0c63ae480c413c60e
kyunglog Kim <kyunglogkim@kyunglogui-MacBookPro.local>
2018-12-05 00:08:50 +0900

    split to full/simple test

--- api/health.go
+++ api/health.go
@@ -2,17 +2,30 @@ package api
 
 import (
 	"net/http"
+	"sync"
 
 	"github.com/labstack/echo"
 )
 
-var isFullCheckDone = false
+var _isFullCheckDone = false
+var _rwMutex = new(sync.RWMutex)
 
 // Health health check
 func Health(c echo.Context) error {
+	_rwMutex.RLock()
+	isFullCheckDone := _isFullCheckDone
+	_rwMutex.RUnlock()
 	if isFullCheckDone {
+		// simple test
 		return c.String(http.StatusOK, "")
 	}
-	// do full function test
+
+	// do full test
+	// ...
+
+	// if succeed full test
+	_rwMutex.Lock()
+	_isFullCheckDone = true
+	_rwMutex.Unlock()
 	return c.String(http.StatusOK, "")
 }
