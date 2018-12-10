b1d7079ee985027dbf32258677508829bde004e0
kyunglog Kim <kyunglogkim@kyunglogui-MacBookPro.local>
2018-12-10 23:41:57 +0900

    fix for use

--- util/crypto.go
+++ util/crypto.go
@@ -16,41 +16,41 @@ func createHash(key string) string {
 }
 
 // Encrypt encrypt string
-func Encrypt(data string, passphrase string) string {
+func Encrypt(data string, passphrase string) (string, error) {
 
 	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
-		panic(err.Error())
+		return "", err
 	}
 	nonce := make([]byte, gcm.NonceSize())
 	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
+		return "", err
 	}
 	ciphertext := gcm.Seal(nonce, nonce, []byte(data), nil)
-	return hex.EncodeToString(ciphertext)
+	return hex.EncodeToString(ciphertext), nil
 }
 
 // Decrypt decrypt string
-func Decrypt(data string, passphrase string) string {
+func Decrypt(data string, passphrase string) (string, error) {
 	dataByte, err := hex.DecodeString(data)
 	if err != nil {
-		panic(err.Error())
+		return "", err
 	}
 	key := []byte(createHash(passphrase))
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err.Error())
+		return "", err
 	}
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
-		panic(err.Error())
+		return "", err
 	}
 	nonceSize := gcm.NonceSize()
 	nonce, ciphertext := dataByte[:nonceSize], dataByte[nonceSize:]
 	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
-		panic(err.Error())
+		return "", err
 	}
-	return string(plaintext)
+	return string(plaintext), nil
 }
