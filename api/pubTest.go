4485fc18283e082fa7ad33235759cc1614461e5a
kyunglog Kim <kyunglogkim@kyunglogui-MacBookPro.local>
2019-01-08 00:42:28 +0900

    to be simple for rate test

--- api/pubTest.go
+++ api/pubTest.go
@@ -9,38 +9,38 @@ import (
 // PubTest test api
 func PubTest(c echo.Context) error {
 
-	// payload scheme
-	type requestScheme struct {
-		Name  string `json:"name" validate:"required"`
-		Msg   string `json:"msg" validate:"required"`
-		Num   int    `json:"num" validate:"required"`
-		Query string `json:"query"`
-	}
+	// // payload scheme
+	// type requestScheme struct {
+	// 	Name  string `json:"name" validate:"required"`
+	// 	Msg   string `json:"msg" validate:"required"`
+	// 	Num   int    `json:"num" validate:"required"`
+	// 	Query string `json:"query"`
+	// }
 	type responseScheme struct {
-		Name  string `validate:"required"`
-		Msg   string `validate:"required"`
-		IP    string `validate:"required"`
-		Num   int    `validate:"required"`
-		Query string
+		// Name  string `validate:"required"`
+		// Msg   string `validate:"required"`
+		IP string `validate:"required"`
+		// Num   int    `validate:"required"`
+		// Query string
 	}
 
-	// verify request payload
-	requestJSON := new(requestScheme)
-	if err := c.Bind(requestJSON); err != nil {
-		return c.String(http.StatusBadRequest, err.Error())
-	}
-	if err := c.Validate(requestJSON); err != nil {
-		return c.String(http.StatusBadRequest, err.Error())
-	}
-	// need error standard
+	// // verify request payload
+	// requestJSON := new(requestScheme)
+	// if err := c.Bind(requestJSON); err != nil {
+	// 	return c.String(http.StatusBadRequest, err.Error())
+	// }
+	// if err := c.Validate(requestJSON); err != nil {
+	// 	return c.String(http.StatusBadRequest, err.Error())
+	// }
+	// // need error standard
 
 	// verify response payload
 	responseJSON := &responseScheme{
-		Name:  requestJSON.Name,
-		Msg:   requestJSON.Msg,
-		Num:   requestJSON.Num,
-		IP:    c.RealIP(),
-		Query: requestJSON.Query,
+		// Name:  requestJSON.Name,
+		// Msg:   requestJSON.Msg,
+		// Num:   requestJSON.Num,
+		IP: c.RealIP(),
+		// Query: requestJSON.Query,
 	}
 	if err := c.Validate(responseJSON); err != nil {
 		return c.String(http.StatusInternalServerError, err.Error())
